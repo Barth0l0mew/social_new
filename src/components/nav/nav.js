b05ad7907552fa83c2dcfe3597f0585e93857431
barth0l0mew <barth0l0mew@mail.ru>
2022-06-27 23:44:42 +0300

    create nav

--- src/components/nav/nav.js
+++ src/components/nav/nav.js
@@ -2,9 +2,17 @@ import React from "react";
 import Style from "./nav.module.css"
 const Nav = ()=>{
   return (
-    <div className={Style.nav}>
-      navbar
-    </div>
+    <nav className={Style.nav}>
+      <ul className={Style.menu}>
+        <li className={Style.menu__item}>Profile</li>
+        <li className={Style.menu__item}>Message</li>
+        <li className={Style.menu__item}>News</li>
+        <li className={Style.menu__item}>Music</li>
+        <li className={Style.menu__item} >Users</li>
+        <li className={Style.menu__item}>Settings</li>
+      </ul>
+      
+    </nav>
   )
 }
 export default Nav
